086479e5ad97ec4bd3d9eb10a1ad71e75aa53bee
Szekeres Alexa <alexaszekeres@outlook.hu>
2022-09-29 12:57:15 +0200

    AutomatedCar-A #7
    Sensor skeleton - a Sensor osztály örökítendő

---
+++ src/AutomatedCar/SystemComponents/Packets/SensorPacket.cs
@@ -0,0 +1,17 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using System.Collections.Generic;
+    using AutomatedCar.Models;
+    using ReactiveUI;
+
+    internal class SensorPacket : ReactiveObject, ISensorPacket
+    {
+        private List<WorldObject> relevantWorldObjs;
+
+        public List<WorldObject> RelevantWorldObjs
+        {
+            get => this.RelevantWorldObjs;
+            set => this.RaiseAndSetIfChanged(ref this.relevantWorldObjs, value);
+        }
+    }
+}
