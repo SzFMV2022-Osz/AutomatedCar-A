b760bed4bc76b0efdb6d28367806308ccb53cfc7
Erős Dániel <erosd99@stud.uni-obuda.hu>
2022-10-03 14:34:42 +0200

    Added method for checking whether a given point is inside a triangle.

--- src/AutomatedCar/Helpers/CollisionDetection.cs
+++ src/AutomatedCar/Helpers/CollisionDetection.cs
@@ -1,8 +1,6 @@
 ﻿namespace AutomatedCar.Helpers
 {
-    using Avalonia.Media;
     using System;
-    using System.Collections.Generic;
     using System.Numerics;
 
     public static class CollisionDetection
@@ -37,7 +35,9 @@
             return dotProduct > 0 && dotProduct <= squaredLineLength;
         }
 
-        private static bool Intersects(Vector2 line1Start, Vector2 line1End, Vector2 line2Start, Vector2 line2End)
+        private static float DotProduct(Vector2 pointA, Vector2 pointB) => (pointA.X * pointB.X) + (pointA.Y * pointB.Y);
+
+        public static bool LinesIntersect(Vector2 line1Start, Vector2 line1End, Vector2 line2Start, Vector2 line2End)
         {
             // Logic: https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
             Orientation o1 = GetOrientation(line1Start, line1End, line2Start);
@@ -70,10 +70,22 @@
             return o4.Equals(Orientation.Collinear) && PointOnLine(line1Start, line2Start, line2End);
         }
 
-        public static bool BoundingBoxesIntersect(PolylineGeometry BoundingBox1, PolylineGeometry BoundingBox2)
+        /// <summary>
+        /// Checks if a 2d point lies in the area specified by the 3 coordinates of the triangle.
+        /// Edge cases:
+        ///     - Point lies in edge of the triangle -> returns true
+        ///     - Point equals one of the points of the triangle -> returns true.
+        /// </summary>
+        /// <param name="point">The point we want to check.</param>
+        /// <param name="triangle">The coordinates of the 3 points of the triangle. Ordering doesn't matter.</param>
+        /// <returns>Whether the point lies inside, or at the edge of the triangle.</returns>
+        public static bool PointInTriangle(Vector2 point, Tuple<Vector2, Vector2, Vector2> triangle)
         {
-            // PolylineGeometry contains Points, which define the bounding box with a Point List. So (p1, p2) defines a line, along with (p2, p3) etc...
-            return true;
+            float dotProduct1 = DotProduct(triangle.Item2 - triangle.Item1, point);
+            float dotProduct2 = DotProduct(triangle.Item3 - triangle.Item2, point);
+            float dotProduct3 = DotProduct(triangle.Item1 - triangle.Item3, point);
+
+            return dotProduct1 >= 0 && dotProduct2 >= 0 && dotProduct3 >= 0;
         }
     }
 }
