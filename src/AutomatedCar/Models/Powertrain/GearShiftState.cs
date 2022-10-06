97f816966855a03830caaba77241cc243e64b6a4
csokispalacsinta <44522566+csokispalacsinta@users.noreply.github.com>
2022-10-06 13:01:32 +0200

    IGearshift interface

---
+++ src/AutomatedCar/Models/Powertrain/GearShiftState.cs
@@ -0,0 +1,7 @@
+﻿namespace AutomatedCar.Models.Powertrain
+{
+    public enum GearshiftState
+    {
+        P, R, N, D
+    }
+}
---
+++ src/AutomatedCar/Models/Powertrain/IGearshift.cs
@@ -0,0 +1,17 @@
+﻿namespace AutomatedCar.Models.Powertrain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IGearshift
+    {
+        void ShiftUp();
+        void ShiftDown();
+        void SetState(GearshiftState state);
+        float GetGearRatio();
+        GearshiftState GetState();
+    }
+}
