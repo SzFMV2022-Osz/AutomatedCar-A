9ee1726487548cd87ad5b53b45fbc9db87907136
Kovács Gábor <31709917+Totable@users.noreply.github.com>
2022-10-06 13:15:36 +0200

    Implement Drag

---
+++ src/AutomatedCar/Models/Powertrain/Engine.cs
@@ -0,0 +1,35 @@
+﻿namespace AutomatedCar.Models.Powertrain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Engine : IEnigne
+    {
+        public int Speed { get; private set; }
+        private int mass;
+        private float dragCoefficient;
+        private float frontArea;
+        public Engine()
+        {
+
+        }
+
+        public void Accelerate()
+        {
+
+        }
+
+        public void Decelerate()
+        {
+
+        }
+
+        private float Drag()
+        {
+            return 0.5f * this.dragCoefficient * this.frontArea * 1.29f;
+        }
+    }
+}
--- src/AutomatedCar/Models/Powertrain/IEnigne.cs
+++ src/AutomatedCar/Models/Powertrain/IEnigne.cs
@@ -11,6 +11,7 @@
         int Speed { get; }
 
         void Accelerate();
+
         void Decelerate();
     }
 }
