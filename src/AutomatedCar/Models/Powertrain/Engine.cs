f3ea147c4c932aca67d10aabf85198dfe6a84ac9
Kovács Gábor <31709917+Totable@users.noreply.github.com>
2022-10-06 14:14:12 +0200

    Update Engine

--- src/AutomatedCar/Models/Powertrain/Engine.cs
+++ src/AutomatedCar/Models/Powertrain/Engine.cs
@@ -11,13 +11,26 @@
         public int Speed { get; private set; }
         private int mass;
         private float dragCoefficient;
+        private float rpm;
         private float frontArea;
         private float crr;
         private float torque;
+        private float diferential;
+        private float transmissionefficiency;
+        private float wheelradius;
+        private float wheelrotationrate;
         private IGearshift gearshift;
-        public Engine()
+        private static float pi = (float)Math.PI;
+
+        public Engine(int mass, float dragCoefficient = 0.30f, float frontArea = 2.2f, float diferential = 3.42f, float transmissionefficiency = 0.7f, float wheelradius = 0.34f)
         {
-            this.crr = 30 * this.Drag();
+            this.mass = mass;
+            this.dragCoefficient = dragCoefficient;
+            this.frontArea = frontArea;
+            this.diferential = diferential;
+            this.transmissionefficiency = transmissionefficiency;
+            this.wheelradius = wheelradius;
+            this.crr = 30 * this.Cdrag();
         }
 
         public void Accelerate()
@@ -30,14 +43,34 @@
 
         }
 
-        private float Drag()
+        private int getRPM()
+        {
+            return (int)(this.wheelrotationrate * this.gearshift.GetGearRatio() * this.diferential * 60 / 2 * pi);
+        }
+
+        private float GetWheelRotationRateByRPM()
+        {
+            return this.rpm / (this.gearshift.GetGearRatio() * this.diferential * 60 / 2 * pi);
+        }
+
+        private float GetWheelRotationRateBySpeed()
+        {
+            return this.Speed / this.wheelradius;
+        }
+
+        private float Cdrag()
         {
             return 0.5f * this.dragCoefficient * this.frontArea * 1.29f;
         }
 
+        private float DragForce()
+        {
+            return 0.5f * this.dragCoefficient * this.frontArea * 1.29f * (this.Speed * this.Speed);
+        }
+
         private float DrivingForce()
         {
-            return 0.0f;
+            return this.torque * this.gearshift.GetGearRatio() * this.diferential * this.transmissionefficiency / this.wheelradius;
         }
     }
 }
