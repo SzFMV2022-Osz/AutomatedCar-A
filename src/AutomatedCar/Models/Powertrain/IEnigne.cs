cd779d626e55474c67c30764c943cbeff71b4827
Kovács Gábor <31709917+Totable@users.noreply.github.com>
2022-10-06 12:58:58 +0200

    IEngine interface

---
+++ src/AutomatedCar/Models/Powertrain/IEnigne.cs
@@ -0,0 +1,16 @@
+﻿namespace AutomatedCar.Models.Powertrain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IEnigne
+    {
+        int Speed { get; }
+
+        void Accelerate();
+        void Decelerate();
+    }
+}
