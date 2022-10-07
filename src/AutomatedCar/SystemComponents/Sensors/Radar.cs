71cb2d32ab7c54cc32c2a818d5197d9421e3561b
egon <gombar.egon@getcon.hu>
2022-10-07 15:22:54 +0200

    Other programmers job

--- src/AutomatedCar/SystemComponents/Sensors/Radar.cs
+++ src/AutomatedCar/SystemComponents/Sensors/Radar.cs
@@ -4,6 +4,7 @@
     using AutomatedCar.SystemComponents.Packets;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Numerics;
 
     public class Radar : Sensor
@@ -14,21 +15,16 @@
 
         public override void Process()
         {
-            this.X = new Vector2(this.GetAutomatedCar().X, this.GetAutomatedCar().Y);
-            this.Y = new Vector2(this.GetAutomatedCar().X + 200, this.GetAutomatedCar().Y + (230.94f / 2));
-            this.Z = new Vector2(this.GetAutomatedCar().X + 200, this.GetAutomatedCar().Y - (230.94f / 2));
         }
 
         protected override List<WorldObject> FilterRelevantWorldObjects()
         {
-            List<WorldObject> filtered = this.FilterObjectsInSensor();
-            return filtered;
+            throw new NotImplementedException();
         }
 
         protected override void SaveWorldObjectsToPacket()
         {
-            this.sensorPacket = new SensorPacket();
-            this.sensorPacket.RelevantWorldObjs = this.FilterRelevantWorldObjects();
+            throw new NotImplementedException();
         }
     }
 }
