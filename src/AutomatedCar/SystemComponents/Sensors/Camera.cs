46b6246d2dbeca8620587d54d37572712bc259ef
egon <gombar.egon@getcon.hu>
2022-10-07 15:27:09 +0200

    Calculate Coordinates

--- src/AutomatedCar/SystemComponents/Sensors/Camera.cs
+++ src/AutomatedCar/SystemComponents/Sensors/Camera.cs
@@ -4,26 +4,35 @@
     using AutomatedCar.SystemComponents.Packets;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Numerics;
 
     public class Camera : Sensor
     {
         public Camera(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
         {
+            this.HorizontalDistance = 50 * 80;
+            this.VerticalDistance = 50 * (92.37f / 2);
+            this.CalculateCoordinates();
         }
 
         public override void Process()
         {
-            this.X = new Vector2(this.GetAutomatedCar().X, this.GetAutomatedCar().Y);
-            this.Y = new Vector2(this.GetAutomatedCar().X + 80, this.GetAutomatedCar().Y + (92.37f / 2));
-            this.Z = new Vector2(this.GetAutomatedCar().X + 80, this.GetAutomatedCar().Y - (92.37f / 2));
+            this.CalculateCoordinates();
+        }
+
+        private void CalculateCoordinates()
+        {
+            this.triangle.X = new Vector2(this.virtualFunctionBus.CarCoordinatesPacket.X + this.HorizontalDistance, this.virtualFunctionBus.CarCoordinatesPacket.Y);
+            this.triangle.Y = new Vector2(this.virtualFunctionBus.CarCoordinatesPacket.X + this.HorizontalDistance, this.virtualFunctionBus.CarCoordinatesPacket.Y + this.VerticalDistance);
+            this.triangle.Z = new Vector2(this.virtualFunctionBus.CarCoordinatesPacket.X + this.HorizontalDistance, this.virtualFunctionBus.CarCoordinatesPacket.Y - this.VerticalDistance);
         }
 
         //Todo: Filter Road, highlight nearest object.
         protected override List<WorldObject> FilterRelevantWorldObjects()
         {
-            List<WorldObject> filtered = this.FilterObjectsInSensor();
-            return filtered;
+            List<WorldObject> objs = this.GetWorldObjects();
+            return objs.Where(obj => this.triangle.X.X <= obj.X && this.triangle.Y.X >= obj.X && this.triangle.Z.Y <= obj.Y && this.triangle.Y.Y >= obj.Y).ToList();
         }
 
         protected override void SaveWorldObjectsToPacket()
