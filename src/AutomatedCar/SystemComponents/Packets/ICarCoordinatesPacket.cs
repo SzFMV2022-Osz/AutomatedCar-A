f8685f19954300e2b10f1f229472ef4fc79e8f34
egon <gombar.egon@getcon.hu>
2022-10-07 15:21:26 +0200

    Created CarCoordinatesPacket

---
+++ src/AutomatedCar/SystemComponents/Packets/ICarCoordinatesPacket.cs
@@ -0,0 +1,15 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface ICarCoordinatesPacket
+    {
+        int X { get; set; }
+
+        int Y { get; set; }
+    }
+}
