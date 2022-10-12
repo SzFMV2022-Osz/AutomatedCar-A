90ae91d871535f878a2a4e0c3772f731ed7af065
DavidGgly <gergelydavid00@gmail.com>
2022-10-12 07:42:00 +0200

    Initial commit

---
+++ src/AutomatedCar/Models/NPC/NpcCar.cs
@@ -0,0 +1,24 @@
+﻿namespace AutomatedCar.Models.NPC
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents the NPC car.
+    /// </summary>
+    internal class NpcCar : NPC
+    {
+        public NpcCar(int x, int y, string filename, int zindex = 1)
+            : base(x, y, filename, zindex, true, WorldObjectType.Car)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void Move()
+        {
+        }
+    }
+}
