397cdab51a54468c51ea064c28f3a6808d81b49f
csokispalacsinta <44522566+csokispalacsinta@users.noreply.github.com>
2022-10-12 19:14:28 +0200

    commented Gearshift

--- src/AutomatedCar/Models/Powertrain/Gearshift.cs
+++ src/AutomatedCar/Models/Powertrain/Gearshift.cs
@@ -6,62 +6,103 @@
     using System.Text;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Gear shift.
+    /// </summary>
     internal class Gearshift : IGearshift
     {
         private GearshiftState state;
         private int gear;
         private float[] ratios = { 0f, 2.66f, 1.78f, 1.3f, 1f, 0.74f, 0.5f };
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Gearshift"/> class.
+        /// Gear shift constuctor.
+        /// </summary>
         public Gearshift()
         {
             this.gear = 0;
             this.state = GearshiftState.P;
         }
 
+        /// <summary>
+        /// Returns gear ratio.
+        /// </summary>
+        /// <returns>ratio.</returns>
         public float GetGearRatio()
         {
-            return ratios[gear];
+            return this.ratios[this.gear];
         }
 
+        /// <summary>
+        /// Returns state.
+        /// </summary>
+        /// <returns>state.</returns>
         public GearshiftState GetState()
         {
-            return state;
+            return this.state;
         }
 
+        /// <summary>
+        /// Returns next gear ratio.
+        /// </summary>
+        /// <returns>ratio or -1f.</returns>
         public float NextGearRatio()
         {
-            if (gear == 6)
+            if (this.gear == 6)
+            {
                 return -1f;
+            }
             else
-                return ratios[gear + 1];
+            {
+                return this.ratios[this.gear + 1];
+            }
         }
 
+        /// <summary>
+        /// Returns previous gear ratio.
+        /// </summary>
+        /// <returns>ratio or -1f.</returns>
         public float PreviousGearRatio()
         {
-            if (gear == 0)
+            if (this.gear == 0)
+            {
                 return -1f;
+            }
             else
-                return ratios[gear - 1];
+            {
+                return this.ratios[this.gear - 1];
+            }
         }
 
+        /// <summary>
+        /// Sets gear shift state.
+        /// </summary>
+        /// <param name="state">state.</param>
         public void SetState(GearshiftState state)
         {
             this.state = state;
         }
 
+        /// <summary>
+        /// Shifts down.
+        /// </summary>
         public void ShiftDown()
         {
-            if(gear > 0)
+            if (this.gear > 0)
             {
-                gear--;
+                this.gear--;
             }
         }
 
+        /// <summary>
+        /// Shifts up.
+        /// </summary>
         public void ShiftUp()
         {
-            if(gear < 6)
+            if (this.gear < 6)
             {
-                gear++;
+                this.gear++;
             }
         }
     }
--- src/AutomatedCar/Models/Powertrain/IGearshift.cs
+++ src/AutomatedCar/Models/Powertrain/IGearshift.cs
@@ -6,14 +6,49 @@
     using System.Text;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// GearShift interface.
+    /// </summary>
     internal interface IGearshift
     {
+        /// <summary>
+        /// Returns next gear ratio.
+        /// </summary>
+        /// <returns>ratio or -1f.</returns>
         float NextGearRatio();
+
+        /// <summary>
+        /// Returns previous gear ratio.
+        /// </summary>
+        /// <returns>ratio or -1f.</returns>
         float PreviousGearRatio();
+
+        /// <summary>
+        /// Shifts up.
+        /// </summary>
         void ShiftUp();
+
+        /// <summary>
+        /// Shifts down.
+        /// </summary>
         void ShiftDown();
+
+        /// <summary>
+        /// Sets gear shift state.
+        /// </summary>
+        /// <param name="state">state.</param>
         void SetState(GearshiftState state);
+
+        /// <summary>
+        /// Returns gear ratio.
+        /// </summary>
+        /// <returns>ratio.</returns>
         float GetGearRatio();
+
+        /// <summary>
+        /// Returns state.
+        /// </summary>
+        /// <returns>state.</returns>
         GearshiftState GetState();
     }
 }
