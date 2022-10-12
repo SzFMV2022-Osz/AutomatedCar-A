7cb6f5330ce215f87a22c40c5777358e16c90e5c
BillyBool <imre.nadas@gmail.com>
2022-10-12 13:27:25 +0200

    Steering class

---
+++ src/AutomatedCar/Models/Steering.cs
@@ -0,0 +1,54 @@
+﻿namespace AutomatedCar.Models
+{
+    using Avalonia;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Reflection.Metadata;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Vector = Avalonia.Vector;
+
+    internal class Steering
+    {
+        //atan(wheelBase / (turningCircle - carWidth)) = turningAngle
+        float wheelBase;
+        double steerAngle;
+
+        Vector carLocation;
+        double carHeading;
+        float carSpeed;
+
+        Vector frontWheel;
+        Vector backWheel;
+
+        float dt;
+
+        public Steering(double steerAngle, int x, int y, float carSpeed)
+        {
+            this.wheelBase = 130f;
+            this.steerAngle = steerAngle;
+            this.carLocation = new Vector(x, y);
+            this.carSpeed = carSpeed;
+        }
+
+        public void findWheelLocations()
+        {
+            frontWheel = carLocation + wheelBase / 2 * new Vector(Math.Cos(carHeading), Math.Sin(carHeading));
+            backWheel = carLocation - wheelBase / 2 * new Vector(Math.Cos(carHeading), Math.Sin(carHeading));
+        }
+
+        public void findNewWheelLocations()
+        {
+            backWheel += carSpeed * dt * new Vector(Math.Cos(carHeading), Math.Sin(carHeading));
+            frontWheel += carSpeed * dt * new Vector(Math.Cos(carHeading + steerAngle), Math.Sin(carHeading + steerAngle));
+        }
+
+        public void getNewHeading()
+        {
+            carLocation = (frontWheel + backWheel) / 2;
+            carHeading = Math.Atan2(frontWheel.Y - backWheel.Y, frontWheel.X - backWheel.X);
+        }
+    }
+}
