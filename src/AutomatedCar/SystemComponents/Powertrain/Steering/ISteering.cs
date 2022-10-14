e9361c7cec4cee6ca7790314c5b05a176e665408
Imi <91874793+nadasimre@users.noreply.github.com>
2022-10-14 22:41:37 +0200

    Refactoring of the Steering class

---
+++ src/AutomatedCar/SystemComponents/Powertrain/Steering/ISteering.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="ISteering.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace AutomatedCar.SystemComponents.Powertrain
+{
+    /// <summary>
+    /// Steering interface.
+    /// </summary>
+    internal interface ISteering
+    {
+        /// <summary>
+        /// Finds the location of the wheels.
+        /// </summary>
+        void FindNewWheelLocations();
+
+        /// <summary>
+        /// Calculates the new wheel locations.
+        /// </summary>
+        void FindWheelLocations();
+
+        /// <summary>
+        /// Calculates the new heading of the car.
+        /// </summary>
+        void GetNewHeading();
+    }
+}
