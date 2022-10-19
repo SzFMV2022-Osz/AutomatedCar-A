72e625b16c46fef3c26194bda18f1cfc114ad825
Norbert Neumann <neumann.norbert.norbert@gmail.com>
2022-10-19 15:24:33 +0200

    Implemented car crash detection

---
+++ src/AutomatedCar/SystemComponents/CarCollisionDetector.cs
@@ -0,0 +1,64 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+    using Avalonia;
+    using Avalonia.Media;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CarCollisionDetector : SystemComponent
+    {
+        private CollisionPacket packet;
+        private IEnumerable<WorldObject> collidableWorldObjects;
+
+        public CarCollisionDetector(VirtualFunctionBus virtualFunctionBus)
+            : base(virtualFunctionBus)
+        {
+            this.packet = new CollisionPacket();
+            this.collidableWorldObjects = World.Instance.WorldObjects
+                                        .Where(obj => obj.Collideable
+                                        && obj != World.Instance.ControlledCar);
+        }
+
+        public override void Process()
+        {
+            var collisions = this.collidableWorldObjects
+                            .Where(obj => CollisionCheck(World.Instance.ControlledCar, obj));
+
+            var dynamicWorldObjects = collisions
+                           .Where(obj => obj.WorldObjectType == WorldObjectType.Car
+                           || obj.WorldObjectType == WorldObjectType.Pedestrian);
+
+            var staticWorldObjects = collisions
+                       .Where(obj => obj.WorldObjectType == WorldObjectType.Building
+                       || obj.WorldObjectType == WorldObjectType.Tree
+                       || obj.WorldObjectType == WorldObjectType.RoadSign);
+
+            this.packet.Collided = collisions.Any();
+            this.packet.CollisionsWithNPCs = dynamicWorldObjects;
+            this.packet.CollisionsWithStaticObjects = staticWorldObjects;
+        }
+
+        private bool CollisionCheck(WorldObject car, WorldObject obj)
+        {
+            PolylineGeometry transformedCarGeometry = TransformGeometry(car);
+            PolylineGeometry transformedObjGeometry = TransformGeometry(obj);
+
+            return Helpers.CollisionDetection.BoundingBoxesCollide(transformedCarGeometry,
+                transformedObjGeometry, 1);
+        }
+
+        // This will be moved to Helpers later
+        private PolylineGeometry TransformGeometry(WorldObject obj)
+        {
+            PolylineGeometry transformedGeometry = new PolylineGeometry(); 
+            foreach (Point point in obj.RawGeometries.First().Points)
+            {
+                transformedGeometry.Points.Add(
+                    new Point(point.X + obj.X, point.Y + obj.Y));
+            }
+            return transformedGeometry;
+        }
+    }
+}
---
+++ src/AutomatedCar/SystemComponents/Packets/IReadOnlyCollisionPacket.cs
@@ -0,0 +1,12 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using AutomatedCar.Models;
+    using System.Collections.Generic;
+
+    public interface IReadOnlyCollisionPacket
+    {
+        bool Collided { get; }
+        IEnumerable<WorldObject> CollisionsWithNPCs { get; }
+        IEnumerable<WorldObject> CollisionsWithStaticObjects { get; }
+    }
+}
