47d97cf3ad91a984fcf871f0389a2e613aa40c73
norbert-neumann <112029577+norbert-neumann@users.noreply.github.com>
2022-10-21 17:40:28 +0200

    Converted angle to radiants before passing to trig functions

--- src/AutomatedCar/Helpers/CollisionDetection.cs
+++ src/AutomatedCar/Helpers/CollisionDetection.cs
@@ -167,6 +167,8 @@
 
         private static Point RotatePoint(Point point, double angle)
         {
+            // Convert to rad
+            angle *= Math.PI / 180f;
             // Rotation Matrix equation: https://en.wikipedia.org/wiki/Rotation_matrix#Common_rotations
             return new Point(
                 (point.X * Math.Cos(angle)) - (point.Y * Math.Sin(angle)),
