04b655bee1950b3b6922539567c213d4721724db
GyJonatan <gyurkovicsjonatan@hotmail.com>
2022-10-23 20:46:01 +0200

    created IPowertrainPacket for common usage

---
+++ src/AutomatedCar/SystemComponents/Packets/IPowertrainPacket.cs
@@ -0,0 +1,13 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    public interface IPowertrainPacket
+    {
+        public int Rpm { get; set; }
+        public int Kmh { get; set; }
+        public int CurrentGear { get; set; }
+        public double ThrottleValue { get; set; }
+        public double BrakeValue { get; set; }
+        public double RotationPoint { get; set; }
+        public int Torque { get; set; }
+    }
+}
