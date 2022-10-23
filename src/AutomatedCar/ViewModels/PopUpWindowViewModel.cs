bd182227d71d6d3660d72f203695f753e1c0269f
KDavidOE <kdavid@stud.uni-obuda.hu>
2022-10-23 22:44:20 +0200

    Added window and viewmodel for car crash popup dialog.

---
+++ src/AutomatedCar/ViewModels/PopUpWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AutomatedCar.ViewModels
+{
+    using AutomatedCar.Models;
+
+    public class PopUpWindowViewModel : ViewModelBase
+    {
+        public PopUpWindowViewModel(AutomatedCar controlledCar)
+        {
+            this.ControlledCar = new CarViewModel(controlledCar);
+        }
+
+        public CarViewModel ControlledCar { get; set; }
+    }
+}
