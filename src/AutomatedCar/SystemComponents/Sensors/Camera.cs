1c1c9902a444dbb419fcfb45de3bfb909afbbd9c
egon <gombar.egon@getcon.hu>
2022-10-24 10:40:06 +0200

    Added DistanceMeasure Filter by road or roadsign Bugfix

--- src/AutomatedCar/SystemComponents/Sensors/Camera.cs
+++ src/AutomatedCar/SystemComponents/Sensors/Camera.cs
@@ -4,6 +4,7 @@
     using AutomatedCar.Models;
     using AutomatedCar.SystemComponents.Packets;
     using Avalonia;
+    using Avalonia.Media;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -25,7 +26,7 @@
 
         private void CalculateCoordinates()
         {
-            this.triangle.X = new Point(this.GetAutomatedCar().X + this.HorizontalDistance, this.GetAutomatedCar().Y);
+            this.triangle.X = new Point(this.GetAutomatedCar().X, this.GetAutomatedCar().Y);
             this.triangle.Y = new Point(this.GetAutomatedCar().X + this.HorizontalDistance, this.GetAutomatedCar().Y + this.VerticalDistance);
             this.triangle.Z = new Point(this.GetAutomatedCar().X + this.HorizontalDistance, this.GetAutomatedCar().Y - this.VerticalDistance);
         }
@@ -33,24 +34,29 @@
         protected override List<WorldObject> FilterRelevantWorldObjects()
         {
             List<WorldObject> objs = this.GetWorldObjects();
-            objs = objs.Where(obj => CollisionDetection.PointInTriangle(new Point(obj.X, obj.Y), new Tuple<Point, Point, Point>(this.triangle.X, this.triangle.Y, this.triangle.Z))).ToList();
-            return objs.OrderBy(t => t.X).ToList();
+            return objs.Where(obj => (obj.WorldObjectType == WorldObjectType.Road || obj.WorldObjectType == WorldObjectType.RoadSign) && CollisionDetection.PointInTriangle(new Point(obj.X, obj.Y), new Tuple<Point, Point, Point>(this.triangle.X, this.triangle.Y, this.triangle.Z))).ToList();
         }
 
         protected override void SaveWorldObjectsToPacket()
         {
-            this.sensorPacket = new SensorPacket();
-            this.sensorPacket.RelevantWorldObjs = this.FilterRelevantWorldObjects();
+            this.virtualFunctionBus.SensorPacket = new SensorPacket();
+            this.virtualFunctionBus.SensorPacket.RelevantWorldObjs = this.FilterRelevantWorldObjects();
+        }
+
+        protected void OrderByDistance()
+        {
+            //distance = sqrt((x2-x1)^2+(y2-y1)^2)
+            this.virtualFunctionBus.SensorPacket.RelevantWorldObjs = this.virtualFunctionBus.SensorPacket.RelevantWorldObjs.OrderBy(obj => Math.Sqrt(Math.Pow((obj.X - this.triangle.X.X), 2) + Math.Pow((obj.Y - this.triangle.X.Y), 2))).ToList();
         }
 
         protected List<WorldObject> FilterRoad()
         {
-            return this.sensorPacket.RelevantWorldObjs.Where(obj => obj.WorldObjectType == WorldObjectType.Road).ToList();
+            return this.virtualFunctionBus.SensorPacket.RelevantWorldObjs.Where(obj => obj.WorldObjectType == WorldObjectType.Road).ToList();
         }
 
         protected WorldObject NearestWorldObject()
         {
-            return this.sensorPacket.RelevantWorldObjs.FirstOrDefault();
+            return this.virtualFunctionBus.SensorPacket.RelevantWorldObjs.FirstOrDefault();
         }
     }
 }
