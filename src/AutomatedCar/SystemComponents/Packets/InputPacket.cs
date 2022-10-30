db18dcacd3cc1c989f85e3378fa6fc027e2173a5
benceb1 <bence.b1.1.2@gmail.com>
2022-10-30 10:55:26 +0100

    Input packed added and implemented

---
+++ src/AutomatedCar/SystemComponents/Packets/InputPacket.cs
@@ -0,0 +1,29 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using AutomatedCar.SystemComponents.InputManager.Messenger;
+
+    public class InputPacket
+    {
+        public SteeringState SteeringState { get; set; }
+
+        public Pedals PedalState { get; set; }
+
+        public Gears GearState { get; set; }
+
+        public bool IsGearStateJustChanged { get; set; }
+
+        public InputPacket()
+        {
+            this.SteeringState = SteeringState.Center;
+            this.PedalState = Pedals.Empty;
+            this.GearState = Gears.Steady;
+            this.IsGearStateJustChanged = false;
+        }
+
+        // for logs
+        public override string ToString()
+        {
+            return $"{this.SteeringState.ToString()} - {this.PedalState.ToString()} - {this.GearState.ToString()} - GearStateJustChanged: {this.IsGearStateJustChanged}";
+        }
+    }
+}
