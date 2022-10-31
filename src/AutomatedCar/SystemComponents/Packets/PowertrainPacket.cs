5d354035c0463b9ed54171b31b8e6133acc2be56
Szűcs Dániel <daniel.szucs@stud.uni-obuda.hu>
2022-10-31 15:25:23 +0100

    Finishing the documentaion of the packet.

--- src/AutomatedCar/SystemComponents/Packets/PowertrainPacket.cs
+++ src/AutomatedCar/SystemComponents/Packets/PowertrainPacket.cs
@@ -1,22 +1,51 @@
-﻿namespace AutomatedCar.SystemComponents.Packets
-{
-    using ReactiveUI;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿// <copyright file="PowertrainPacket.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
+namespace AutomatedCar.SystemComponents.Packets
+{
+    using AutomatedCar.SystemComponents.InputManager.Messenger;
+    using AutomatedCar.SystemComponents.Powertrain;
+    using ReactiveUI;
+
+    /// <summary>
+    /// Packet for the VFB to communicate with other systems.
+    /// </summary>
     public class PowertrainPacket : ReactiveObject, IPowertrainPacket
     {
+        /// <summary>
+        /// Gets or sets the percentage of the gas pedal.
+        /// </summary>
+        public int CurrentThrottleValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets the percentage of the brake pedal.
+        /// </summary>
+        public int CurrentBrakeValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current speed of the car in Km/h.
+        /// </summary>
+        public int CurrentSpeed { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current gearbox state. Values: P,R,N,D.
+        /// </summary>
+        public GearshiftState CurrentGear { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current revolution of the engine.
+        /// </summary>
         public int Rpm { get; set; }
 
-        // speed
-        public int Kmh { get; set; }
-        public int CurrentGear { get; set; }
-        public double ThrottleValue { get; set; }
-        public double BrakeValue { get; set; }
-        public double RotationPoint { get; set; }
-        public int Torque { get; set; }
+        /// <summary>
+        /// Gets or sets the current steering state. Values: Left, Right, Center.
+        /// </summary>
+        public SteeringState Steering { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rotation of the steering wheel.
+        /// </summary>
+        public double RotationAngle { get; set; }
     }
 }
