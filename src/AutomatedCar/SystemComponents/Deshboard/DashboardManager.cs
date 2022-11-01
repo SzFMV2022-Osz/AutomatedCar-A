623c0ae952cdb841aeb2f0721ce2056c6e9eb918
Ponti1999 <albert.dvid@gmail.com>
2022-11-01 21:03:35 +0100

    ID-17 Dashboard in progress.

---
+++ src/AutomatedCar/SystemComponents/Deshboard/DashboardManager.cs
@@ -0,0 +1,49 @@
+﻿namespace AutomatedCar.SystemComponents.Dashboard
+{
+    using AutomatedCar.SystemComponents.Packets;
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class DashboardManager : SystemComponent
+    {
+        private IPowertrainPacket powertrainPacket;
+
+        public event EventHandler PropertyChanged;
+        private int rpm;
+
+        public int Rpm
+        {
+            get { return rpm; }
+            set
+            {
+                rpm = value;
+                this.PropertyChanged?.Invoke(this, null);
+            }
+        }
+
+        public int CurrentSpeed { get; set; }
+        public int CurrentBrakeValue { get; set; }
+        public int CurrentThrottleValue { get; set; }
+        public double RotationAngle { get; set; }
+        public string CurrentGear { get; set; }
+        public string Steering { get; set; }
+
+        public DashboardManager(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+            this.powertrainPacket = new PowertrainPacket();
+            virtualFunctionBus.PowertrainPacket = this.powertrainPacket;
+        }
+
+        public override void Process()
+        {
+            Rpm = this.virtualFunctionBus.PowertrainPacket.Rpm;
+            CurrentSpeed = this.virtualFunctionBus.PowertrainPacket.CurrentSpeed;
+            CurrentBrakeValue = this.virtualFunctionBus.PowertrainPacket.CurrentBrakeValue;
+            CurrentThrottleValue = this.virtualFunctionBus.PowertrainPacket.CurrentThrottleValue;
+            RotationAngle = this.virtualFunctionBus.PowertrainPacket.RotationAngle;
+            CurrentGear = this.virtualFunctionBus.PowertrainPacket.CurrentGear.ToString();
+            Steering = this.virtualFunctionBus.PowertrainPacket.Steering.ToString();
+        }
+    }
+}
