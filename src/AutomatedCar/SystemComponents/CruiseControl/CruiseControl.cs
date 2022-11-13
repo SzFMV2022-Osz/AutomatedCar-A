e15b4234ba2e88788a23a9eddfd773f4a6d9f5bc
vaghylevente <vaghylevente@stud.uni-obuda.hu>
2022-11-13 13:16:23 +0100

    CruiseControl.cs added

---
+++ src/AutomatedCar/SystemComponents/CruiseControl/CruiseControl.cs
@@ -0,0 +1,20 @@
+﻿namespace AutomatedCar.SystemComponents.CruiseControl
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CruiseControl : SystemComponent
+    {
+        public CruiseControl(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+        }
+
+        public override void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
