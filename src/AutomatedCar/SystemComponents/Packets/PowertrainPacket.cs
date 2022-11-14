a68fec5acaeef023c24fd734eaf784269890ad70
GyJonatan <gyurkovicsjonatan@hotmail.com>
2022-11-14 13:42:22 +0100

    added rpm, kmh and steering wheel rotation bindings to the DashboardView.xaml

---
+++ src/AutomatedCar/SystemComponents/Packets/PowertrainPacket.cs
@@ -0,0 +1,19 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using System;
+
+    public class PowertrainPacket : IPowertrainPacket
+    {
+        public int Rpm { get; set; }
+        public int Kmh { get; set; }
+        public int CurrentGear { get; set; }
+        public double ThrottleValue { get; set; }
+        public double BrakeValue { get; set; }
+        public double RotationPoint { get; set; }
+        public int Torque { get; set; }
+        public PowertrainPacket(int inputRpm)
+        {
+            Rpm = inputRpm; //example of usage
+        }
+    }
+}
