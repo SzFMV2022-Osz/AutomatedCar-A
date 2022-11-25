8fd5f0c1f39e52925d4338140b3323f02cdeac24
GyJonatan <gyurkovicsjonatan@hotmail.com>
2022-11-25 10:50:12 +0100

    validation finished

--- src/AutomatedCar/SystemComponents/LaneKeepingAssistant/Validation/LKAValidation.cs
+++ src/AutomatedCar/SystemComponents/LaneKeepingAssistant/Validation/LKAValidation.cs
@@ -15,7 +15,7 @@
             int carX = 25, carY = 25, carSpeed = 130;
 
             worldObjects = worldObjects.Where(x => x.Type == "RoadSide");
-            return roadSideIsOK(carX, carY) && speedIsOK(carSpeed);
+            return roadSideIsOK(carX, carY)/* && speedIsOK(carSpeed)*/;
         }
 
         public async Task<bool> MustBeTurnedOff(IEnumerable<worldObject> objects)
@@ -31,7 +31,40 @@
 
         private bool curveIsNotOK()
         {
-            throw new NotImplementedException();
+            List<worldObject> objects = worldObjects.ToList();
+            objects.OrderBy(x => x.X + x.Y);
+            int squaredLikeObjectsCount = 0;
+            for (int i = 0; i < objects.Count-1; i++)
+            {
+                bool isSquaredLikeByX = isSquaredLike(objects[i].X, objects[i + 1].X);
+                bool isSquaredLikeByY = isSquaredLike(objects[i].Y, objects[i + 1].Y);
+                if (isSquaredLikeByX || isSquaredLikeByY)
+                {
+                    squaredLikeObjectsCount++;
+                }
+            }
+
+            if (squaredLikeObjectsCount >= objects.Count * 0.8)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        bool isSquaredLike(int x1, int x2)
+        {
+            bool result = false;
+            int squaredX1 = Convert.ToInt32(Math.Pow(x1, 2));
+            int squaredX2 = Convert.ToInt32(Math.Pow(x2, 2));
+            int squaredDifference = Math.Abs(squaredX1 - squaredX2);
+            int normalDifference = Math.Abs(x1 - x2);
+            int normalDifferenceSquared = Convert.ToInt32(Math.Pow(normalDifference, 2));
+            if (Convert.ToInt32(Math.Pow(normalDifferenceSquared, 2)) >= squaredDifference * 0.8)
+            {
+                result = true;
+            }
+            return result;
         }
 
         bool roadSideIsOK(int carX, int carY)
@@ -42,21 +75,21 @@
             // option 2: there are roadsides in front of as for x meters away.
             // if we decide to do this, then we'd need the current position of our car. I'm going to use a simple carX, carY here
             // but it should be fed into the function alongside the list of worldObjects.
-            bool enoughDistance = false;
-            int predefinedDistance = 50; // tbd
-            worldObject furthestAway = worldObjects.OrderByDescending(x => x.X + x.Y).First();
+            //bool enoughDistance = false;
+            //int predefinedDistance = 50; // tbd
+            //worldObject furthestAway = worldObjects.OrderByDescending(x => x.X + x.Y).First();
 
-            if (furthestAway.X + furthestAway.Y - carX + carY > predefinedDistance)
-                enoughDistance = true;
+            //if (furthestAway.X + furthestAway.Y - carX + carY > predefinedDistance)
+            //    enoughDistance = true;
 
-            return enoughRoadSides || enoughDistance;
+            return enoughRoadSides /*|| enoughDistance*/;
         }
 
-        bool speedIsOK(int carSpeed)
-        {
-            // tbd: is our current speed relevant to the lane keeping assistant? I'd say its not, just to shave off some time, but if it is, then:
-            int predefinedSpeed = 120;
-            return carSpeed < predefinedSpeed;
-        }
+        //bool speedIsOK(int carSpeed)
+        //{
+        //    // tbd: is our current speed relevant to the lane keeping assistant? I'd say its not, just to shave off some time, but if it is, then:
+        //    int predefinedSpeed = 120;
+        //    return carSpeed < predefinedSpeed;
+        //}
     }
 }
