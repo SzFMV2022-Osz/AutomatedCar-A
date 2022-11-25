ec05a3c5eb2fc0da1f698bb38bfebc9dc160e007
popolopo21 <89850285+popolopo21@users.noreply.github.com>
2022-11-25 17:01:00 +0100

    Implement target speed

--- src/AutomatedCar/SystemComponents/CruiseControl/CruiseControl.cs
+++ src/AutomatedCar/SystemComponents/CruiseControl/CruiseControl.cs
@@ -12,6 +12,11 @@
 
         private readonly double[] accDistances = new double[4] { 0.8, 1, 1.2, 1.4 };
         private int currentAccDistanceIdx;
+ 
+        private int targetSpeed;
+        private int targetSpeedDiff = 10;
+        private readonly int minTargetSpeed = 30;
+        private readonly int maxTargetSpeed = 160;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CruiseControl"/> class.
@@ -22,7 +27,8 @@
         {
             this.ACCenabled = false;
             this.currentAccDistanceIdx = 0;
-        }
+            this.targetSpeed = 0;
+    }
 
         /// <summary>
         /// Gets the current ACC distance value (e.g 0.8). OverflowException is being avoided using modulus.
@@ -52,16 +58,64 @@
                     if (World.Instance.ControlledCar.cruiseControl.ACCenabled)
                     {
                         World.Instance.ControlledCar.cruiseControl.ACCenabled = !World.Instance.ControlledCar.cruiseControl.ACCenabled;
+                        this.targetSpeed = 0;
                     }
                     else if (!World.Instance.ControlledCar.cruiseControl.ACCenabled && World.Instance.ControlledCar.VirtualFunctionBus.PowertrainPacket.CurrentSpeed >= 30)
                     {
                         World.Instance.ControlledCar.cruiseControl.ACCenabled = !World.Instance.ControlledCar.cruiseControl.ACCenabled;
+                        if (World.Instance.ControlledCar.VirtualFunctionBus.PowertrainPacket.CurrentSpeed >= 160)
+                        {
+                            this.targetSpeed = this.maxTargetSpeed;
+                        }
+                        else
+                        {
+                            this.targetSpeed = World.Instance.ControlledCar.VirtualFunctionBus.PowertrainPacket.CurrentSpeed;
+                        }
                     }
 
                     ControlMessenger.Instance.FireCruiseControlEvent(CruiseControlInputs.Empty);
                     break;
+
                 case CruiseControlInputs.ChangeTargetDistance:
                     this.SetNextAccDistance();
+                    break;
+
+                case CruiseControlInputs.IncreaseTargetSpeed:
+                    if (this.ACCenabled)
+                    {
+                        if (World.Instance.ControlledCar.VirtualFunctionBus.PowertrainPacket.CurrentSpeed + this.targetSpeedDiff >= this.maxTargetSpeed)
+                        {
+                            this.targetSpeed = this.maxTargetSpeed;
+                        }
+                        else if (World.Instance.ControlledCar.VirtualFunctionBus.PowertrainPacket.CurrentSpeed + this.targetSpeedDiff <= this.minTargetSpeed)
+                        {
+                            this.targetSpeed = this.minTargetSpeed;
+                        }
+                        else
+                        {
+                            this.targetSpeed += this.targetSpeedDiff;
+                        }
+                    }
+
+                    break;
+
+                case CruiseControlInputs.DecreaseTargetSpeed:
+                    if (this.ACCenabled)
+                    {
+                        if (World.Instance.ControlledCar.VirtualFunctionBus.PowertrainPacket.CurrentSpeed - this.targetSpeedDiff >= this.maxTargetSpeed)
+                        {
+                            this.targetSpeed = this.maxTargetSpeed;
+                        }
+                        else if (World.Instance.ControlledCar.VirtualFunctionBus.PowertrainPacket.CurrentSpeed - this.targetSpeedDiff <= this.minTargetSpeed)
+                        {
+                            this.targetSpeed = this.minTargetSpeed;
+                        }
+                        else
+                        {
+                            this.targetSpeed -= this.targetSpeedDiff;
+                        }
+                    }
+
                     break;
             }
         }
