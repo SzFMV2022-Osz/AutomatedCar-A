9fcfd476e455aa4401bd5eef3d93a2a49f23778f
GyJonatan <gyurkovicsjonatan@hotmail.com>
2022-11-27 23:37:43 +0100

    added several checks for LKA validation

---
+++ src/AutomatedCar/SystemComponents/LaneKeepingAssistant/Validation/ILKAValidation.cs
@@ -0,0 +1,19 @@
+﻿namespace AutomatedCar.SystemComponents.LaneKeepingAssistant.Validation
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface ILKAValidation
+    {
+        class Coordinate
+        {
+            public int X { get; set; }
+
+            public int Y { get; set; }
+        }
+
+        bool CanBeTurnedOn(List<Coordinate> objects, int carX, int carY);
+
+        bool MustBeTurnedOff(List<Coordinate> worldObjects);
+    }
+}
