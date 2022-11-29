cdaae91a6c4a841f0c1150f12def47cabd971ac8
vaghylevente <vaghylevente@stud.uni-obuda.hu>
2022-11-29 17:16:49 +0100

    ACC packet created

---
+++ src/AutomatedCar/SystemComponents/Packets/CruiseControlPacket.cs
@@ -0,0 +1,34 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using ReactiveUI;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CruiseControlPacket : ReactiveObject
+    {
+        private bool accEnabled;
+        private int targetSpeed;
+        private int targetDistance;
+
+        public bool ACCEnabled
+        {
+            get => true;
+            set => this.RaiseAndSetIfChanged(ref this.accEnabled, value);
+        }
+
+        public int TargetSpeed
+        {
+            get => 0;
+            set => this.RaiseAndSetIfChanged(ref this.targetSpeed, value);
+        }
+
+        public int TargetDistance
+        {
+            get => 0;
+            set => this.RaiseAndSetIfChanged(ref this.targetDistance, value);
+        }
+    }
+}
